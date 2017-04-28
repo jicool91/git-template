48630e711caa7fda64853bd0df0f63ad7a169240
jicool91 <jicool912@gmail.com>
2017-04-28 20:34:33 +0300

    header done

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<? include "top-row.php"?>
+	<? include "header.php"?>
+</body>
+
+</html>
---
+++ top-row.php
@@ -0,0 +1,14 @@
+<div class="top-row">
+	<div class="container">
+		<div class="top-row__menu">
+			<div class="top-row__text">
+				<span class="top-row__phone">Tel: xxxxx xxxxxxxxxx | </span>
+				<span class="top-row__email">Mail: info@domain.com</span>
+			</div>
+			<div class="top-row__search">
+				<input type="text" class="search" placeholder="search term here">
+				<button class="top-row__search-button">Search</button>
+			</div>
+		</div>
+	</div>
+</div>
